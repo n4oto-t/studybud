86dc13412eadb5a4b029567fdad3057d99899bea
N4oto-T <h13.only57@gmail.com>
2024-09-23 21:58:00 +0900

    CRUD

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name=""),
+    path("", views.home, name="home"),
     # name='room'とすることでurlが"room_hoge/<str:pk>/"に変わっても、template側でも同様にroom_hogeと変更する必要がなくなる。
     path("room/<str:pk>/", views.room, name="room"),
+    path("create_room/", views.create_room, name="create_room"),
+    path("update_room/<str:pk>", views.update_room, name="update_room"),
+    path("delete_room/<str:pk>", views.delete_room, name="delete_room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
+from .forms import RoomForm
 from .models import Room
 
 # rooms = [
@@ -19,3 +21,37 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")  ## urls.pyのpath引数のnameを指定。
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+    return render(request, "base/delete.html", {"obj": room})
