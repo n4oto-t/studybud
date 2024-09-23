8f7aaea5dd8ef3c966715029bba09e4e1878b617
N4oto-T <h13.only57@gmail.com>
2024-09-23 16:59:19 +0900

    Templates

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.home, name=""), path("room/", views.room, name="room")]
+urlpatterns = [
+    path("", views.home, name=""),
+    # name='room'とすることでurlが"room_hoge/<str:pk>/"に変わっても、template側でも同様にroom_hogeと変更する必要がなくなる。
+    path("room/<str:pk>/", views.room, name="room"),
+]
--- base/views.py
+++ base/views.py
@@ -1,10 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+rooms = [
+    {"id": 1, "name": "Lets learn python!"},
+    {"id": 2, "name": "Design with me !"},
+    {"id": 3, "name": "Lets learn golang!"},
+]
 
 
 def home(request):
-    return HttpResponse("home page")
+    return render(request, "base/home.html", {"rooms": rooms})
 
 
-def room(request):
-    return HttpResponse("Room")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, "base/room.html", context)
