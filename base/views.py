4b10091776dd24695280e3fbc34d514ecf303409
N4oto-T <h13.only57@gmail.com>
2024-09-24 19:26:07 +0900

    Login

--- base/views.py
+++ base/views.py
@@ -1,14 +1,37 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import redirect, render
 
 from .forms import RoomForm
 from .models import Room, Topic
 
+
 # rooms = [
 #     {"id": 1, "name": "Lets learn python!"},
 #     {"id": 2, "name": "Design with me !"},
 #     {"id": 3, "name": "Lets learn golang!"},
 # ]
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or password does not exist")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
 
 
 def home(request):
