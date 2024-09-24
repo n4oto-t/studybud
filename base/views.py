d17bff9e81577cdc13636db90911a08c218e267c
N4oto-T <h13.only57@gmail.com>
2024-09-24 19:03:00 +0900

    Search

--- base/views.py
+++ base/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import redirect, render
 
 from .forms import RoomForm
-from .models import Room
+from .models import Room, Topic
 
 # rooms = [
 #     {"id": 1, "name": "Lets learn python!"},
@@ -12,8 +12,15 @@ from .models import Room
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get("q") if request.GET.get("q") is not None else ""
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
